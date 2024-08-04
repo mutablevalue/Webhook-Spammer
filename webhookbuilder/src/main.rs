c9a7a0a16bacfa9a3136ca16a6bfa255a978949a
Santana <153578858+mutablevalue@users.noreply.github.com>
2024-08-04 04:15:47 -0500

    Add files via upload

---
+++ webhookbuilder/src/main.rs
@@ -0,0 +1,27 @@
+use serenity::all::ExecuteWebhook;
+use serenity::all::Http;
+use serenity::all::Webhook;
+use serenity::Result;
+use std::io::{self, Read};
+use tokio;
+
+const content: &str = "";
+const username: &str = "";
+const webhook_data: &str = "";
+const send_amount: i32 = 5;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let http = Http::new("");
+    let webhook = Webhook::from_url(&http, webhook_data).await?;
+    let builder = ExecuteWebhook::new().content(content).username(username);
+    let mut amount: i32 = 0;
+    loop {
+        webhook.execute(&http, false, builder.clone()).await?;
+        amount += 1;
+        if amount >= send_amount {
+            break;
+        }
+    }
+    Ok(())
+}
